f8c96104f91ae0792ddd76d37e3b68ae12484372
koichi <bbcoow_k11@yahoo.co.jp>
2026-07-04 12:29:26 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-const CACHE_NAME = "tokyo-table-flow-v1";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "tokyo-table-flow-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css?v=3",
+  "./app.js?v=3",
+  "./manifest.webmanifest",
+  "./icon.svg",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
